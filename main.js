f16eed9a4ee6a9f9243751781968ebb018b68fdd
vitor monteiro <vitor_891@hotmail.com>
2023-04-10 21:09:14 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,45 @@
+const form = document.getElementById('form-agenda');
+let linhas = '';
+const contatos = [];
+const telefones = [];
+
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    adicionaLinha();
+    atualizaTabela();
+});
+
+function adicionaLinha() {
+
+    const inputNome = document.getElementById('nome');
+    const inputNumero = document.getElementById('numero');
+
+    if (contatos.includes(inputNome.value)) {
+        alert (`O contato ${inputNome.value} já foi inserido`);
+    }
+    else{
+
+        if (telefones.includes(parseInt(inputNumero.value))){
+            alert (`O número ${inputNumero.value} já pertence à um contato`);
+        }
+        else{
+        contatos.push(inputNome.value);
+        telefones.push(parseInt(inputNumero.value));
+    
+        let linha = '<tr>';
+        linha += `<td>${inputNome.value}</td>`;
+        linha += `<td>${inputNumero.value}</td>`;
+        linha += '</tr>';
+    
+        linhas += linha;
+        }
+    }
+
+    inputNome.value = '';
+    inputNumero.value = '';
+}
+function atualizaTabela (){
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
